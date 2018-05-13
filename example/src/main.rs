19ee25904749856f99a6e77940dd6fc0223b3e8b
Dan Glastonbury <dan.glastonbury@gmail.com>
2018-05-14 09:50:37 +1000

    Update example to demonstrate tracing to .vcd files.

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,29 +1,51 @@
 #![feature(proc_macro)]
+#![feature(duration_extras)]
 
 extern crate verilated;
 extern crate verilated_module;
+
+use std::time::Duration;
+
 use verilated_module::module;
-use verilated::test_bench::TestBench;
 
 #[module(top)]
 pub struct Top {
-    #[port(clock)] pub clk_i: bool,
-    #[port(reset)] pub rst_i: bool,
-    #[port(output)] pub count_o: [bool; 4],
+    #[port(clock)]
+    pub clk_i: bool,
+    #[port(reset)]
+    pub rst_i: bool,
+    #[port(output)]
+    pub count_o: [bool; 4],
 }
 
 fn main() {
-    let mut tb = TestBench::<Top>::init(|core, tick_count| {
-        if tick_count > 10 {
-            return false;
+    let mut tb: Top = Top::default();
+    tb.eval();
+    tb.eval();
+
+    tb.open_trace("counter.vcd", 99).unwrap();
+
+    let mut clocks: u64 = 0;
+    while tb.count_o() < 10 {
+        if clocks == 0 {
+            tb.reset_toggle();
+        } else if clocks == 2 {
+            tb.reset_toggle();
         }
 
-        println!("{}: count_o = {}", tick_count, core.count_o());
+        tb.clock_toggle();
+        tb.eval();
+        tb.trace_at(Duration::from_nanos(20 * clocks));
 
-        true
-    });
+        tb.clock_toggle();
+        tb.eval();
+        tb.trace_at(Duration::from_nanos(20 * clocks + 10));
 
-    while !tb.done() {
-        tb.tick();
+        println!("{}: count_o = {}", clocks, tb.count_o());
+
+        clocks += 1;
     }
+    tb.trace_at(Duration::from_nanos(20 * clocks));
+
+    tb.finish();
 }
