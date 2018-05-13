875011cbea4b067a883bb4ea7c5dac34f25d1ed5
Dan Glastonbury <dan.glastonbury@gmail.com>
2018-05-14 09:48:49 +1000

    Support GCC and Clang flags.

--- verilated/build.rs
+++ verilated/build.rs
@@ -37,21 +37,35 @@ fn main() {
         let files: Vec<PathBuf> = files.iter().map(|p| include.join(p)).collect();
 
         let mut cfg = cc::Build::new();
-        cfg.cpp(true)
-            .target(&target)
-            .flag("-faligned-new")
-            .flag("-fbracket-depth=4096")
-            .flag("-Qunused-arguments")
-            .flag("-Wno-parentheses-equality")
-            .flag("-Wno-sign-compare")
-            .flag("-Wno-uninitialized")
-            .flag("-Wno-unused-parameter")
-            .flag("-Wno-unused-variable")
-            .flag("-Wno-shadow")
-            .include(&include)
+        let tool = cfg.get_compiler();
+        cfg.cpp(true).target(&target);
+        if tool.is_like_clang() {
+            cfg.flag("-faligned-new")
+                .flag("-fbracket-depth=4096")
+                .flag("-Qunused-arguments")
+                .flag("-Wno-parentheses-equality")
+                .flag("-Wno-sign-compare")
+                .flag("-Wno-uninitialized")
+                .flag("-Wno-unused-parameter")
+                .flag("-Wno-unused-variable")
+                .flag("-Wno-shadow");
+        }
+        if tool.is_like_gnu() {
+            cfg.flag("-std=gnu++17")
+                .flag("-faligned-new")
+                .flag("-Wno-bool-operation")
+                .flag("-Wno-sign-compare")
+                .flag("-Wno-uninitialized")
+                .flag("-Wno-unused-but-set-variable")
+                .flag("-Wno-unused-parameter")
+                .flag("-Wno-unused-variable")
+                .flag("-Wno-shadow");
+        }
+        cfg.include(&include)
             .include(include.join("vltstd"))
             .files(files)
-            .file("src/verilated_shim.cpp");
+            .file("src/verilated_shim.cpp")
+            .file("src/verilatedvcdc_shim.cpp");
         cfg.compile("verilated_all");
     } else {
         fail("Failed to find `${VERILATOR_ROOT}`.  Please set `VERILATOR_ROOT` environment variable or ensure `verilator` is in `PATH`.");
