64f43da6ad8897bc4f2104d3c040c2125ef9e600
Dan Glastonbury <dan.glastonbury@gmail.com>
2018-05-14 09:46:49 +1000

    Fix up the logic in find_verilator_root to return the correct path.

--- verilator/src/lib.rs
+++ verilator/src/lib.rs
@@ -11,31 +11,35 @@ pub mod gen;
 pub mod module;
 
 use std::env;
-use std::ffi::OsString;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
+fn check_verilator_bin(path: &Path) -> bool {
+    path.join("verilator_bin").is_file()
+}
+
 fn check_verilator_root(root: &Path) -> bool {
-    root.join("verilator").is_file()
+    root.join("include/verilated.cpp").is_file()
 }
 
 pub fn find_verilator_root() -> Option<PathBuf> {
-    env::var_os("VERILATOR_ROOT")
-        .map(|p| PathBuf::from(p).join("bin"))
-        .into_iter()
-        .chain(env::split_paths(&env::var_os("PATH")
-            .unwrap_or(OsString::new())))
-        .find(|p| check_verilator_root(p.as_ref()))
+    env::split_paths(&env::var_os("PATH").unwrap_or_default())
+        .find(|p| check_verilator_bin(p.as_ref()))
         .and_then(|p| {
-            let mut cmd = Command::new(p.join("verilator"));
+            let mut cmd = Command::new(p.join("verilator_bin"));
             cmd.arg("--getenv").arg("VERILATOR_ROOT");
 
-            match cmd.output() {
+            cmd.output().ok().map(|output| {
                 // Report what verilator says is ${VERILATOR_ROOT}
-                Ok(output) => Some(PathBuf::from(
-                    String::from_utf8_lossy(&output.stdout).trim(),
-                )),
-                Err(..) => p.parent().map(|p| p.join("share")),
+                PathBuf::from(String::from_utf8_lossy(&output.stdout).trim())
+            })
+        })
+        .or_else(|| env::var("VERILATOR_ROOT").map(PathBuf::from).ok())
+        .and_then(|p| {
+            if check_verilator_root(p.as_ref()) {
+                Some(p)
+            } else {
+                None
             }
         })
 }
